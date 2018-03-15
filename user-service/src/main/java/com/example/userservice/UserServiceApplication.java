deccce015a9657f5b8eeb83ac871a47ef618deb9
Levin RAJU M <l.raju@everteam.com>
2018-03-15 16:22:56 +0530

    oauth2 working

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,11 +3,11 @@ package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableEurekaClient
 @SpringBootApplication
-//@EnableResourceServer
+@EnableResourceServer
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
